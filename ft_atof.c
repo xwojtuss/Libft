af3f3b46e2466e39f7cfc3498bb9b1a4a659865e
xwojtuss <wojkor338@gmail.com>
2024-08-20 12:39:46 +0200

    atof working

--- ft_atof.c
+++ ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: wkornato <wkornato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 10:25:16 by wkornato          #+#    #+#             */
-/*   Updated: 2024/08/20 11:39:06 by wkornato         ###   ########.fr       */
+/*   Updated: 2024/08/20 12:38:56 by wkornato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,27 +29,27 @@ double	ft_atof(const char *str)
 {
 	double	result;
 	int		i;
-	bool	is_negative;
+	int		offset;
 
 	i = 0;
 	result = 0;
 	if (!str)
 		return (0);
-	is_negative = (str[0] == '-' && i++ == 0);
-	result = get_part(str, &i);
+	if (str[0] == '-')
+		i++;
+	result = ft_atoi(str + i);
+	i += ft_intlen_d(ft_atoi(str));
 	if (str[i] == 'e' || str[i] == 'E')
 	{
-		i++;
-		result *= ft_pow(10, ft_atoi(str + i));
+		result *= ft_pow(10, ft_atoi(str + ++i));
+		i += ft_intlen(ft_atoi(str + i));
+		return (result * ((str[0] != '-') * 2 - 1));
 	}
-	if (!str[i] || str[i] != '.')
-		return (result * (!is_negative * 2 - 1));
-	i++;
-	result += (double)get_part(str, &i) / ft_pow(10, ft_intlen_d(ft_atoi(str)));
+	if (!str[i] || str[i] != '.' || !str[i + 1])
+		return (result * ((str[0] != '-') * 2 - 1));
+	offset = ft_intlen_d(ft_atoi(str + ++i));
+	result += (double)get_part(str, &i) / ft_pow(10, offset);
 	if (str[i] == 'e' || str[i] == 'E')
-	{
-		i++;
-		result *= ft_pow(10, ft_atoi(str + i));
-	}
-	return (result * (!is_negative * 2 - 1));
+		result *= ft_pow(10, ft_atoi(str + ++i));
+	return (result * ((str[0] != '-') * 2 - 1));
 }
