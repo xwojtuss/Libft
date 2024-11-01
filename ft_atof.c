c103bcec9fdf69dabca00749a8fed735bee458da
Wojtek Kornatowski <wojkor338@gmail.com>
2024-11-01 12:17:51 +0100

    fixed -0.0something not calculating properly

--- ft_atof.c
+++ ft_atof.c
@@ -3,24 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atof.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wkornato <wkornato@student.42.fr>          +#+  +:+       +#+        */
+/*   By: wkornato <wkornato@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 10:25:16 by wkornato          #+#    #+#             */
-/*   Updated: 2024/08/20 12:38:56 by wkornato         ###   ########.fr       */
+/*   Updated: 2024/11/01 12:13:22 by wkornato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	get_part(const char *str, int *i)
+static int	get_part(const char *str, int *i, int *offset)
 {
 	int	int_part;
 
 	int_part = 0;
+	*offset = 0;
 	while (ft_isdigit(str[*i]))
 	{
 		int_part = int_part * 10 + (str[*i] - '0');
 		(*i)++;
+		(*offset)++;
 	}
 	return (int_part);
 }
@@ -47,8 +49,8 @@ double	ft_atof(const char *str)
 	}
 	if (!str[i] || str[i] != '.' || !str[i + 1])
 		return (result * ((str[0] != '-') * 2 - 1));
-	offset = ft_intlen_d(ft_atoi(str + ++i));
-	result += (double)get_part(str, &i) / ft_pow(10, offset);
+	i++;
+	result += (double)get_part(str, &i, &offset) / ft_pow(10, offset);
 	if (str[i] == 'e' || str[i] == 'E')
 		result *= ft_pow(10, ft_atoi(str + ++i));
 	return (result * ((str[0] != '-') * 2 - 1));
